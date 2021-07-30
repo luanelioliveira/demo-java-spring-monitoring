89614f749bba316f1e53e46e73d8cda6bdae47ea
Luan Eli Oliveira <luannn@gmail.com>
2021-07-30 10:20:34 -0300

    add project

---
+++ src/test/java/io/github/luanelioliveira/monitoring/DemoJavaSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luanelioliveira.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaSpringMonitoringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
